9c0aad81e0ebc46218058fbb3f8250f10e642a51
dewdrew02 <peeradon1559@gmail.com>
2026-01-20 09:39:37 +0700

    ...

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,10 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaLibSql } from '@prisma/adapter-libsql'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  const url = process.env.DATABASE_URL || 'file:./dev.db'
+  const adapter = new PrismaLibSql({ url })
+  return new PrismaClient({ adapter })
 }
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
